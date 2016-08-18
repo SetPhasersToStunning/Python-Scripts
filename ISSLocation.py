b89fe5e6dc84ffe2c8ce788456fcbd2cce6031b6
SetPhasersToStunning <bplosion@gmail.com>
2016-08-18 16:06:58 +0100

    Added number of people in space

--- ISSLocation.py
+++ ISSLocation.py
@@ -13,24 +13,37 @@ her_number = '+447526512854'
 #api for ISS location
 req = urllib2.Request("http://api.open-notify.org/iss-now.json")
 response = urllib2.urlopen(req)
+obj1 = json.loads(response.read())
 
-obj = json.loads(response.read())
+#api for number of astros in space
+req = urllib2.Request("http://api.open-notify.org/astros.json")
+response = urllib2.urlopen(req)
+obj2 = json.loads(response.read())
+
+number = (int(obj2['number']))
+
 
 #returned values
-timestamp = ('Timestamp: ' + str(obj['timestamp']))
-latitude = (str(obj['iss_position']['latitude']))
-longitude = (str(obj['iss_position']['longitude']))
+timestamp = ('Timestamp: ' + str(obj1['timestamp']))
+latitude = (str(obj1['iss_position']['latitude']))
+longitude = (str(obj1['iss_position']['longitude']))
 
 locationtext = ('Lat: ' + latitude + 'Long: ' + longitude)
 
+print
 print (timestamp)
 print (locationtext)
+print
+print('Number of people in space: ' + str(number))
+
 
 
 #send text message
+'''
 client.messages.create(
     to=her_number,
     from_=my_number,
     body= locationtext)
+'''
 
 print("Message sent!")
